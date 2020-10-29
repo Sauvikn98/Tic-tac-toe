3205c9ca9deff6404fcd9af4005ed7064d2fad35
Sauvik Nath <sauviknath10@gmail.com>
2020-10-29 10:16:09 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b65f710d1d05b494d28a0ec1b92a2f5f",
-"/": "b65f710d1d05b494d28a0ec1b92a2f5f",
+"index.html": "2a499852befce00aee54fe485f667b80",
+"/": "2a499852befce00aee54fe485f667b80",
 "main.dart.js": "0b9ccc59afbb75ecb327019e1c685216",
 "manifest.json": "42734c8f50fa0ffab80e272e1b881f43"
 };
